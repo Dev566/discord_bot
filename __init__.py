25dacf85cccdd6b6d2889acbc9282e69fdc2d2eb
Debabrata <dev.566@gmail.com>
2024-08-16 01:09:50 -0700

    [added bot for website status]
    website update bot

--- __init__.py
+++ __init__.py
@@ -54,6 +54,7 @@ import os
 from dotenv import load_dotenv , dotenv_values
 from discordBot import discord_bot_run
 # from scrapper import process_and_analyse
+from botWebsiteChecker import website_summary_bot_run
 load_dotenv()
 
 app = Flask(__name__)
@@ -78,15 +79,25 @@ def run_discord_bot():
     
     asyncio.set_event_loop(new_loop)
     new_loop.run_until_complete(discord_bot_run(my_variable))
+    
+def run_website_status():
+    new_loop = asyncio.new_event_loop()
+    my_variable = os.getenv('DISCORD_TOKEN', 'default_value')
+    
+    asyncio.set_event_loop(new_loop)
+    new_loop.run_until_complete(website_summary_bot_run(my_variable))
 
 if __name__ == '__main__':
     # Run Flask app and Discord bot in separate threads
     flask_thread = threading.Thread(target=run_flask)
     discord_thread = threading.Thread(target=run_discord_bot)
+    web_status_thread = threading.Thread(target=run_website_status)
     
     flask_thread.start()
     discord_thread.start()
+    web_status_thread.start()
     
     flask_thread.join()
     discord_thread.join()
+    web_status_thread.join()
  
---
+++ botWebsiteChecker.py
@@ -0,0 +1,67 @@
+# IMPORT DISCORD.PY. ALLOWS ACCESS TO DISCORD'S API.
+import discord
+from datetime import datetime, timedelta
+import time
+import requests
+import asyncio
+
+# IMPORT THE OS MODULE.
+import os
+
+intents = discord.Intents.default()
+bot = discord.Client(intents=intents)
+time_interval = 60
+VIVY_CHANNEL_ID = 1271030253392760916
+
+async def check_website_status(url):
+    try:
+        response = requests.get(url)
+        # Check if the status code indicates success (200 OK)
+        if response.status_code == 200:
+            return "The website " + url+" is up and running."
+        else:
+            return "The website  " + url+"  returned status code {response.status_code}."
+    except requests.exceptions.RequestException as e:
+        return f'The website **{url}** is down!'
+
+
+async def send_message_to_channel(chanel_id, message='Hello!'):
+    print(f'Logged in as {bot.user.name}')
+    channel = bot.get_channel(chanel_id)
+    if channel is not None:
+        await channel.send(message)  # Send your message here
+    else:
+        print('Channel not found')
+
+# EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
+@bot.event
+async def on_ready():
+    await send_message_to_channel(VIVY_CHANNEL_ID, "I will in testing phase. Testing 3 hour status update.")
+    while not bot.is_closed():
+        print("Checking status")
+        status_message = await check_website_status("https://dev.waku-travel.com/")
+        print("Checked status")
+        await send_message_to_channel(VIVY_CHANNEL_ID,status_message)
+        print("Message send")
+        # Wait for 3 hours (10800 seconds) before checking again
+        await asyncio.sleep(10800)  # Sleep for 3 hours   
+
+
+async def check_website_status_loop():
+    await bot.wait_until_ready()
+    
+
+    while not bot.is_closed():
+        status_message = check_website_status("https://dev.waku-travel.com/")
+
+        await send_message_to_channel(VIVY_CHANNEL_ID,status_message)
+        
+        # Wait for 3 hours (10800 seconds) before checking again
+        await asyncio.sleep(10800)  # Sleep for 3 hours        
+
+# EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
+
+def website_summary_bot_run(discord_bot_key):
+    # bot.loop.create_task(check_website_status_loop())
+    # check_website_status_loop()
+    bot.run(discord_bot_key)
